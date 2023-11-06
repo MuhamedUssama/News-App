83b759c6d67a7d11c03c389317070bb421086405
Mohamed <mohamed.osama200538@gmail.com>
2023-11-06 18:24:28 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/ui/screens/Home/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeDesign.routeName: (_) => const HomeDesign(),
+      },
+      initialRoute: HomeDesign.routeName,
+    );
+  }
+}
---
+++ lib/ui/screens/Home/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeDesign extends StatelessWidget {
+  static const routeName = "home";
+  const HomeDesign({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
