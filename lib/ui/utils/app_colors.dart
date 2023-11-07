d88971780ccdba325178ff8d1ad696b458d0fe69
Mohamed <mohamed.osama200538@gmail.com>
2023-11-07 12:10:30 +0200

    update AppTheme

--- lib/ui/utils/app_colors.dart
+++ lib/ui/utils/app_colors.dart
@@ -5,6 +5,8 @@ abstract class AppColors {
   static const Color importantText = Color(0xff303030);
   static const Color categoryText = Color(0xff4F5A69);
   static const Color textColor = Color(0xff42505C);
+  static const Color sourcesUnderArticle = Color(0xff79828B);
+  static const Color timeColor = Color(0xffA3A3A3);
   static const Color white = Colors.white;
   static const Color transparent = Colors.transparent;
 }
---
+++ lib/ui/utils/app_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/ui/utils/app_colors.dart';
+
+abstract class AppTheme {
+  static const TextStyle appBarAndCategoriesTitle = TextStyle(
+      fontSize: 22, fontWeight: FontWeight.w400, color: AppColors.white);
+
+  static const TextStyle sourcesTab = TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w400, color: AppColors.white);
+
+  static const TextStyle articalText = TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w500, color: AppColors.textColor);
+
+  static const TextStyle sourcesUnderArticle = TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      color: AppColors.sourcesUnderArticle);
+
+  static const TextStyle timeText = TextStyle(
+      fontSize: 13, fontWeight: FontWeight.w400, color: AppColors.timeColor);
+
+  static const TextStyle drawerText = TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      color: AppColors.importantText);
+
+  static const TextStyle settingLanguageText = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: AppColors.importantText);
+
+  static const TextStyle searchText = TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w400, color: AppColors.primary);
+
+  static const TextStyle articleDescriptionText = TextStyle(
+      fontSize: 13, fontWeight: FontWeight.w300, color: AppColors.textColor);
+}
