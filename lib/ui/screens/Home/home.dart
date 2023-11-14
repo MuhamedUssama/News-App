6bde1b3c965591892b052d515a60a84741cd91ec
Mohamed <mohamed.osama200538@gmail.com>
2023-11-14 06:20:45 +0200

    change backButton function

--- lib/ui/screens/Home/home.dart
+++ lib/ui/screens/Home/home.dart
@@ -16,39 +16,53 @@ class HomeDesign extends StatefulWidget {
 
 class _HomeDesignState extends State<HomeDesign> {
   late Widget currentClick;
+  late Widget categoiesTab;
 
   @override
   void initState() {
     super.initState();
 
-    currentClick = CategoryTab(onCategoryClicked: onCategoryClick);
+    categoiesTab = CategoryTab(onCategoryClicked: onCategoryClick);
+    currentClick = categoiesTab;
   }
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("News App", style: AppTheme.appBarAndCategoriesTitle),
-        centerTitle: true,
-        backgroundColor: AppColors.primary,
-        elevation: 0,
-        toolbarHeight: height * 0.085,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(50),
-          ),
-        ),
-      ),
-      body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(AppAssets.background),
-              fit: BoxFit.fill,
+    return WillPopScope(
+      onWillPop: () async {
+        if (currentClick != categoiesTab) {
+          currentClick = CategoryTab(onCategoryClicked: onCategoryClick);
+          setState(() {});
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title:
+              const Text("News App", style: AppTheme.appBarAndCategoriesTitle),
+          centerTitle: true,
+          backgroundColor: AppColors.primary,
+          elevation: 0,
+          toolbarHeight: height * 0.085,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(50),
             ),
           ),
-          child: currentClick),
+        ),
+        body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppAssets.background),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: currentClick),
+      ),
     );
   }
 
